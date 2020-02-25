7590985f6c033f365fbb6196eb41a446f1df0e77
adicore <56226709+adicore@users.noreply.github.com>
2020-02-25 11:08:49 +0700

    Create theme.js

---
+++ travelacer/js/theme.js
@@ -0,0 +1,16 @@
+	/*----------------------------------------------------*/
+	/*  MailChimp Slider
+    /*----------------------------------------------------*/
+	function mailChimp() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	}
+	mailChimp();
+	function mailChimp2() {
+		$('#mc_embed_signup2').find('form').ajaxChimp();
+	}
+	mailChimp2();
+
+	/*----------------------------------------------------*/
+	/*  Nice Select js 
+    /*----------------------------------------------------*/
+	$('select').niceSelect();
