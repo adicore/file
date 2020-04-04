06f42fdf7042e6f2948255a6c36782290f9f1c5c
adicore <56226709+adicore@users.noreply.github.com>
2020-04-05 06:17:35 +0700

    Update reCaptcha.php

--- keystroke/php/reCaptcha.php
+++ keystroke/php/reCaptcha.php
@@ -1,1 +1,12 @@
-
+$captcha = $_POST['g-recaptcha-response'];
+if (!$captcha) {
+  echo 'Please check the captcha form.';
+} else {
+  $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lcix-YUAAAAADeDzQ_UVJsNtootz2OrAGHdI8C0&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+  if ($response.success != true) {
+    echo 'You are spammer ! Get the @$%K out!!';
+  } else {
+    echo 'Congrat! You are not spammer.'
+    // Return your proccess code form here.
+  }
+}
