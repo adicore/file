dffde07dd1765c46431711c103ce987911b4d2d4
adicore <56226709+adicore@users.noreply.github.com>
2020-04-09 08:57:34 +0700

    Create index.php

---
+++ hanyatestingan/testws/index.php
@@ -0,0 +1,22 @@
+<?php
+// koneksi ke database SQLite3
+require 'db.php';
+
+// ambil data
+$json = array();
+$select_str = 'SELECT * FROM arsip';
+
+// filter data berdasarkan kata kunci pencarian
+$filter = filter_input(INPUT_GET, 'katakunci');
+if ($filter) {
+    $select_str .= " WHERE kode LIKE '$filter%' OR judul LIKE '%$filter%'";
+}
+$query_data = $pdo->query($select_str);
+while ($row = $query_data->fetch(PDO::FETCH_ASSOC)) {
+    $json[] = $row;
+}
+
+// tampilkan data dalam format json
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+echo json_encode($json); die();
