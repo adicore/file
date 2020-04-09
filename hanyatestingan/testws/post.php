eb5acfd0b5cfc287fbd7d71baa4ff7dfc627b609
adicore <56226709+adicore@users.noreply.github.com>
2020-04-09 09:09:14 +0700

    Update post.php

--- hanyatestingan/testws/post.php
+++ hanyatestingan/testws/post.php
@@ -1,6 +1,6 @@
 <?php
 // koneksi ke database SQLite3
-require 'db.php';
+require 'https://cdn.statically.io/gh/adicore/file/2ce1e930/hanyatestingan/testws/db.php';
 
 // masukkan data
 $post_data = file_get_contents('php://input');
