346901e7e345401f4de70b7ee7d39706d4c534c4
adicore <56226709+adicore@users.noreply.github.com>
2020-04-09 08:58:00 +0700

    Create post.php

---
+++ hanyatestingan/testws/post.php
@@ -0,0 +1,23 @@
+<?php
+// koneksi ke database SQLite3
+require 'db.php';
+
+// masukkan data
+$post_data = file_get_contents('php://input');
+$input = json_decode($post_data);
+if (is_object($input)) {
+    $sth = $pdo->prepare('REPLACE INTO arsip VALUES (:kode, :judul)');
+    $data = array(':kode' => $input->kode, ':judul' => $input->judul);
+    $sth->execute($data);
+
+    $json = array('status' => 'sukses');
+} else {
+    $json = array('status' => 'gagal');
+}
+
+// tampilkan data dalam format json
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: POST');
+header('Content-type: application/json');
+echo json_encode($json); die();
