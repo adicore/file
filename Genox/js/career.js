bdee94c156b872af0454ee29b16b2e5290147ae9
adicore <56226709+adicore@users.noreply.github.com>
2020-05-16 19:19:34 +0700

    Update career.js

--- Genox/js/career.js
+++ Genox/js/career.js
@@ -71,12 +71,12 @@ simpleCart({
 //Create Option Shipping Check
 $(document).ready(function () {
 	function b() {
-		$("#ContactFormA .contact-form-error-message").hide();
-		$("#ContactFormA .contact-form-success-message").hide();
-		var a = $("#ContactFormA .contact-form-name").val(),
-		b = $("#ContactFormA .contact-form-email").val(),
-		c = $("#ContactFormA .contact-form-number").val(),
-		d = $("#ContactFormA .contact-form-website").val(),
+		$("#ContactForm3 .contact-form-error-message").hide();
+		$("#ContactForm3 .contact-form-success-message").hide();
+		var a = $("#ContactForm3 .contact-form-name").val(),
+		b = $("#ContactForm3 .contact-form-email").val(),
+		c = $("#ContactForm3 .contact-form-number").val(),
+		d = $("#ContactForm3 .contact-form-website").val(),
 		k = $("#raintemplates-contact-form-message").val(),
 		g = $(".contact-form-company").val(),
 		h = "",
@@ -90,9 +90,9 @@ $(document).ready(function () {
 		var e = "";
 		$("#paypal").is(":checked") && (e = "Paypal");
 		$("#cashde").is(":checked") && (e = "Cash On Delivery");
-		$(".infobasic-3").html('<textarea class="contact-form-email-message" cols="25" id="ContactFormA_contact-form-email-message" name="email-message" rows="5"></textarea>');
+		$(".infobasic-3").html('<textarea class="contact-form-email-message" cols="25" id="ContactForm3_contact-form-email-message" name="email-message" rows="5"></textarea>');
 		a = "Payment Method:" + e + "\n_____________________________________\n\nName :____________:" + a + "\nEmail :____________:" + b + "\nPhone Number :____:" + c + "\nAddress :__________:" + d + "\nRegion :___________:" + k + "\nCompany :_________:" + g + "\n\nCustomer's Order:\n_____________________________________\n\n" + h + "_____________________________________\nGrand Total:" + r + "\n_____________________________________";
-		$("#ContactFormA .contact-form-email-message").val(a)
+		$("#ContactForm3 .contact-form-email-message").val(a)
 	}
 	function f() {
 		var a = $(".contact-wrapper .contact-form-name").val(),
@@ -133,12 +133,12 @@ $(document).ready(function () {
 	}
 	function n() {
 		var a = !0;
-		$("#ContactFormA .contact-form-email").each(function () {
+		$("#ContactForm3 .contact-form-email").each(function () {
 			if (l(this.value)) $(this).closest(".form-section").find(".message-show").hide();
 			else return $(this).closest(".form-section").find(".message-show").show(),
 			a = !1
 		});
-		$("#ContactFormA .eachrequire").each(function () {
+		$("#ContactForm3 .eachrequire").each(function () {
 			var b = this.value;
 			if ("" == b || null == b) return $(this).closest(".form-section").find(".message-show").show(),
 			a = !1;
@@ -252,7 +252,7 @@ $(document).ready(function () {
 		n() ? ($(".cartbuttons").append('<a class="simpleCart_checkout" style="display:none" href="javascript:;">Checkout</a>'), b(), $("html, body").animate({
 			scrollTop: $(".infobasic-2").offset().top
 		},
-		1200), $(".simpleCart_checkout").click(), $("#ContactFormA .contact-form-button-submit").click()) : $("html, body").animate({
+		1200), $(".simpleCart_checkout").click(), $("#ContactForm3 .contact-form-button-submit").click()) : $("html, body").animate({
 			scrollTop: $(".infobasic-1").offset().top - 120
 		},
 		500)
@@ -333,7 +333,7 @@ $(document).ready(function () {
 					Error("Something went badly wrong!")
 				})
 			}
-		}), b(), $("#ContactFormA .contact-form-button-submit").click(), $("html, body").animate({
+		}), b(), $("#ContactForm3 .contact-form-button-submit").click(), $("html, body").animate({
 			scrollTop: $(".infobasic-2").offset().top
 		},
 		1200), window.location.replace(successpage_cashondelivery)) : $("html, body").animate({
